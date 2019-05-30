31a98b4d1d59aa95fbb4fa1cf60141df33037612
Katya Gonina <kgonina@google.com>
2019-05-30 09:59:15 -0700

    reverse paths

--- webp_build/builder.py
+++ webp_build/builder.py
@@ -11,7 +11,7 @@ conan, _, _ = conan_api.ConanAPIV1.factory()
 
 # Use Conan to install libwebp
 with tempfile.TemporaryDirectory() as tmp_dir:
-    conan.install(path=getcwd(), cwd=tmp_dir)
+    conan.install(path=tmp_dir, cwd=getcwd())
     with open(path.join(tmp_dir, 'conanbuildinfo.json'), 'r') as f:
         conan_info = json.load(f)
 
